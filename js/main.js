ef73cf9b16b5e1486545fe7363b3c7e8cf03c698
Matan Bakshi <matan.bakshi@gmail.com>
2019-06-13 11:02:42 +0300

    implemented DOM values retreiving

--- js/main.js
+++ js/main.js
@@ -1,3 +1,15 @@
 let game = new Game();
 
 console.log(game);
+
+
+$(document).ready(() => {
+    $(".cell").click((e) => {
+        let target = e.target;
+
+        let x = target.getAttribute("x");
+        let y = target.getAttribute("y");
+
+        alert(`I clicked the cell ${x},${y}`)
+    });
+});
